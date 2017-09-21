f9df4704bd9902b7637f6d04aac5e8ca96e1e5a9
explance <lentsalts@gmail.com>
2017-09-21 23:43:22 +0100

    Location is now a function, no longer multiple query in same class

--- src/com/veil/Core.java
+++ src/com/veil/Core.java
@@ -4,32 +4,64 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+
 public class Core
-        extends JavaPlugin
-{
+        extends JavaPlugin {
     public String PREFIX = ChatColor.DARK_RED + "[SECURITY] ";
     private static Core instance;
     private DB db;
 
-    public static Core getInstance()
-    {
+    public static Core getInstance() {
         return instance;
     }
 
-    public DB getDB()
-    {
+    public DB getDB() {
         return this.db;
     }
 
-    public void onEnable()
-    {
+    public void onEnable() {
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         instance = this;
         this.db = new DB();
         Bukkit.getServer().getPluginManager().registerEvents(new JoinHandler(), this);
         getCommand("auth").setExecutor(new IpResetCommand());
+    }
+
+    public static HashMap<String, String> getLoc(String ip) {
+        HashMap<String, String> toReturn = new HashMap<>();
+
+
+        try {
+            URL url = new URL("http://ip-api.com/line/" + ip);
+            URLConnection connection = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String verify = in.readLine();
+            String line2 = in.readLine();
+            String line3 = in.readLine();
+            String line4 = in.readLine();
+            String line5 = in.readLine();
+            String city = in.readLine();
+            /*String line7 = in.readLine();
+            String line8 = in.readLine();
+            String line9 = in.readLine();
+            String line10 = in.readLine();
+            String line11 = in.readLine();
+            String organisation = in.readLine();*/
+            toReturn.put("city", city);
+            toReturn.put("verify", verify);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return toReturn;
     }
 }
--- src/com/veil/JoinHandler.java
+++ src/com/veil/JoinHandler.java
@@ -21,129 +21,67 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class JoinHandler
-        implements Listener
-{
-    @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+        implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void join(PlayerJoinEvent event) {
-        if (!event.getPlayer().hasPermission("auth.required")) {
+        Player p = event.getPlayer();
+        if (!p.hasPermission("auth.required")) {
             return;
         }
 
-        if (Core.getInstance().getDB().getIp(event.getPlayer()) == null) {
-            try {
 
-                URL url = new URL("http://ip-api.com/line/" + event.getPlayer().getAddress().getHostName());
-                URLConnection connection = url.openConnection();
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line1 = in.readLine();
-                String line2 = in.readLine();
-                String line3 = in.readLine();
-                String line4 = in.readLine();
-                String line5 = in.readLine();
-                String city = in.readLine();
-                String line7 = in.readLine();
-                String line8 = in.readLine();
-                String line9 = in.readLine();
-                String line10 = in.readLine();
-                String line11 = in.readLine();
-                String organisation = in.readLine();
+        // String city  = Core.getLoc(p.getAddress().getHostName()).getOrDefault("city", "nocity"); //This avoids nullpointer exception if there was an error. U can use this:
+        String verify = Core.getLoc(p.getAddress().getHostName()).get("verify");
+        String city = Core.getLoc(p.getAddress().getHostName()).get("city");
 
-                if (line1 != null) {
-                    if (line1 == "fail") {
-                        event.getPlayer().kickPlayer(Core.getInstance().PREFIX + ChatColor.RED + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#1");
-                        return;
-                    }
-                    Core.getInstance().getDB().setIp(event.getPlayer(), city);
-                    event.getPlayer().sendMessage(Core.getInstance().PREFIX + ChatColor.GREEN + "You have been authenticated for the first time.");
-                    return;
+        p.sendMessage(city);
+        p.sendMessage(verify);
 
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+        if (Core.getInstance().getDB().getIp(p) == null) {
+            if (!verify.equalsIgnoreCase("success")) {
+                p.kickPlayer(Core.getInstance().PREFIX + ChatColor.RED + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#1 >" + verify);
+                Bukkit.getConsoleSender().sendMessage(verify);
+                return;
             }
+            Core.getInstance().getDB().setIp(p, city);
+            p.sendMessage(Core.getInstance().PREFIX + ChatColor.GREEN + "You have been authenticated for the first time.");
+            return;
 
         }
-        if (Core.getInstance().getDB().getResetQueue().contains(event.getPlayer().getUniqueId().toString())) {
-            try {
-
-                URL url = new URL("http://ip-api.com/line/" + event.getPlayer().getAddress().getHostName());
-                URLConnection connection = url.openConnection();
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line1 = in.readLine();
-                String line2 = in.readLine();
-                String line3 = in.readLine();
-                String line4 = in.readLine();
-                String line5 = in.readLine();
-                String city = in.readLine();
-                String line7 = in.readLine();
-                String line8 = in.readLine();
-                String line9 = in.readLine();
-                String line10 = in.readLine();
-                String line11 = in.readLine();
-                String organisation = in.readLine();
+        if (Core.getInstance().getDB().getResetQueue().contains(p.getUniqueId().toString())) {
 
 
-                if (line1 != null) {
-                    if (line1 == "fail") {
-                        event.getPlayer().kickPlayer(Core.getInstance().PREFIX + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#2");
-                        return;
-                    }
-                    event.getPlayer().sendMessage(
-                            Core.getInstance().PREFIX + ChatColor.GREEN + "An admin has authenticated you since last login.");
-                    Core.getInstance().getDB().setIp(event.getPlayer(), city);
-                    Core.getInstance().getDB().delFromResetQueue(event.getPlayer());
-                    return;
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (verify != "success") {
+                p.kickPlayer(Core.getInstance().PREFIX + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#2");
+                return;
             }
+            p.sendMessage(
+                    Core.getInstance().PREFIX + ChatColor.GREEN + "An admin has authenticated you since last login.");
+            Core.getInstance().getDB().setIp(p, city);
+            Core.getInstance().getDB().delFromResetQueue(p);
+            return;
 
         }
 
-        try {
 
-            URL url = new URL("http://ip-api.com/line/" + event.getPlayer().getAddress().getHostName());
-            URLConnection connection = url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line1 = in.readLine();
-            String line2 = in.readLine();
-            String line3 = in.readLine();
-            String line4 = in.readLine();
-            String line5 = in.readLine();
-            String city = in.readLine();
-            String line7 = in.readLine();
-            String line8 = in.readLine();
-            String line9 = in.readLine();
-            String line10 = in.readLine();
-            String line11 = in.readLine();
-            String organisation = in.readLine();
-
-            if (line1 != null) {
-                if (line1 == "fail") {
-                    event.getPlayer().kickPlayer(ChatColor.RED + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#3");
-                    return;
-                }
-                if (!Core.getInstance().getDB().getIp(event.getPlayer()).equals(city)) {
-                    event.getPlayer().kickPlayer(
-                            Core.getInstance().PREFIX + "\n" + ChatColor.GRAY + "(" + event.getPlayer().getName() + ")" + "\n" + ChatColor.RED + "You have failed to pass VeilMC authentication protocols." + "\n" + ChatColor.YELLOW + "This system is to prevent un-authorised access to accounts.");
-                    for (Player abc : Bukkit.getOnlinePlayers()) {
-                        if(abc.hasPermission("auth.required")) {
-                            abc.sendMessage(" ");
-                            abc.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + event.getPlayer().getName() + ChatColor.RED + "" + ChatColor.BOLD + " failed login security authentication");
-                            abc.sendMessage(" ");
-
-                        }
-                    }
-                    return;
-                }
-                event.getPlayer().sendMessage(Core.getInstance().PREFIX + ChatColor.GREEN + "Authentication successful");
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!verify.equalsIgnoreCase("success")) {
+            p.kickPlayer(ChatColor.RED + "An error occured, please contact developer with " + ChatColor.DARK_RED + "#3");
+            return;
         }
+        if (!Core.getInstance().getDB().getIp(p).equals(city)) {
+            p.kickPlayer(
+                    Core.getInstance().PREFIX + "\n" + ChatColor.GRAY + "(" + p.getName() + ")" + "\n" + ChatColor.RED + "You have failed to pass VeilMC authentication protocols." + "\n" + ChatColor.YELLOW + "This system is to prevent un-authorised access to accounts.");
+            for (Player abc : Bukkit.getOnlinePlayers()) {
+                if (abc.hasPermission("auth.required")) {
+                    abc.sendMessage(" ");
+                    abc.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + p.getName() + ChatColor.RED + "" + ChatColor.BOLD + " failed login security authentication");
+                    abc.sendMessage(" ");
 
-
+                }
+            }
+            return;
+        }
+        p.sendMessage(Core.getInstance().PREFIX + ChatColor.GREEN + "Authentication successful");
     }
 }
